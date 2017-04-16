81f04a21e2406df9ba7ada034a6814d767498d1a
Eibriel <eibriel@yahoo.com.ar>
2017-04-16 00:17:56 -0300

    :q

--- synth/modules/main/__init__.py
+++ synth/modules/main/__init__.py
@@ -6,6 +6,7 @@ from flask import Blueprint
 from flask import render_template
 #from flask import g
 
+import re
 #import json
 #import time
 #import requests
@@ -19,15 +20,76 @@ main = Blueprint('main', __name__)
 
 #from difflib import SequenceMatcher
 
+text = """
+# Example
+
+H:Hi there
+H:Buenas
+    R:How are you?
+    R:¿Cómo estás?
+        H:I'm fine and you?
+        H:Bien ¿Y tu?
+            R:My systems are working as expected {systems:ok}
+            R:Mis sistemas están funcionando según lo esperado {systems:ok}
+        H:I'm sad {sadness:0.9}
+        H:Estoy triste {sadness:0.9}
+            R:What happened?
+            R:¿Qué sucedió?
+    R:Long time not see you {days_away:2}
+    R:Hace mucho que no te veo {days_away:2}
+        H:True! Did you miss me?
+        H:Cierto! Me extrañaste?
+    R:What do you want? {angry:0.7}
+    R:¿Qué querés? {angry:0.7}
+"""
+
+tags = {
+    "start":            "Conversation start command",
+    "silence":          "An empty muted answer",
+    "eos":            "A new line",
+
+    "name":             "A name",
+    "language":         "A language",
+    "switch":           "Mark a change on the subject of the conversation",
+
+    "typo":             "A typo=Where are you {typo}form{}from{/typo}?",
+
+    "request":          "A request={request}Make me a sandwich{/request}",
+    "confirmation":     "A confirmation={confirmation}Yes please{/confirmation}",
+    "compare":          "A confirmation={compare}{subject:1}Beach{/subject} or {subject:2}mountains{/subject}?{/compare}",
+
+    "taxa":             "A taxa (race)=I am a {taxa}robot{/taxa}",
+    "ice_breaker":      "Short question to bring a new theme",
+    "greetings":        "Greetings={greetings}Hi there{/greetings}",
+
+    "subject":          "What are we talking about?",
+    "subject_owner":    "Who is the owner of that subject?",
+    "subject_property": "A property of the subject=The {subject}sun{/subject} is {subject_property}hot{/subject_property}",
+    "interest":         "Interest=I like {interest}footbal{/interest}",
+
+    "satisfaction":     "Emotions",
+    "surprise":         "Emotions",
+    "curiosity":        "Emotions",
+    "happyness":        "Emotions",
+    "tender":           "Emotions",
+}
+
+def parse_synth(text):
+    parsed = {"not_found": []}
+    tag_search = re.findall(r"\{(?:\/)?(?P<tag>.*?)(?:\:.*?)?(?:\/)?\}", text)
+    for found_tag in tag_search:
+        if found_tag not in tags:
+            parsed["not_found"].append(found_tag)
+    return parsed
+
+
 @main.route('/api')
 def api():
+    with open('synth_files/0fac76.synth', 'r') as myfile:
+        text=myfile.read()
     test = {
-        "text": "#Example\nH:Hi\nR:Hi there[!]",
-        "parsed": [
-                {"text": "Example", "from": ""},
-                {"text": "Hi", "from": "H"},
-                {"text": "Hi there[!]", "from": "R", "entities": {"optional":[[8, 11]]}}
-            ]
+        "text": text,
+        "parsed": parse_synth(text)
     }
     return jsonify(test)
 
---
+++ synth/static/ace-noconflict/mode-synth.js
@@ -0,0 +1,145 @@
+ace.define("ace/mode/matching_brace_outdent",["require","exports","module","ace/range"], function(require, exports, module) {
+"use strict";
+
+var Range = require("../range").Range;
+
+var MatchingBraceOutdent = function() {};
+
+(function() {
+
+    this.checkOutdent = function(line, input) {
+        if (! /^\s+$/.test(line))
+            return false;
+
+        return /^\s*\}/.test(input);
+    };
+
+    this.autoOutdent = function(doc, row) {
+        var line = doc.getLine(row);
+        var match = line.match(/^(\s*\})/);
+
+        if (!match) return 0;
+
+        var column = match[1].length;
+        var openBracePos = doc.findMatchingBracket({row: row, column: column});
+
+        if (!openBracePos || openBracePos.row == row) return 0;
+
+        var indent = this.$getIndent(doc.getLine(openBracePos.row));
+        doc.replace(new Range(row, 0, row, column-1), indent);
+    };
+
+    this.$getIndent = function(line) {
+        return line.match(/^\s*/)[0];
+    };
+
+}).call(MatchingBraceOutdent.prototype);
+
+exports.MatchingBraceOutdent = MatchingBraceOutdent;
+});
+
+ace.define("ace/mode/synth_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/text_highlight_rules"], function(require, exports, module) {
+"use strict";
+
+var oop = require("../lib/oop");
+var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
+
+var SynthHighlightRules = function() {
+   this.$rules = {
+        "start" : [
+            {
+                token: "comment", // String, Array, or Function: the CSS token to apply
+                regex: /^\s*#.*/, // String or RegExp: the regexp to match
+            },
+            {
+                token: "string", // String, Array, or Function: the CSS token to apply
+                regex: /H(\(\d+\))?:/, // String or RegExp: the regexp to match
+            },
+            {
+                token: "variable", // String, Array, or Function: the CSS token to apply
+                regex: /R(\(\d+\))?:/, // String or RegExp: the regexp to match
+            },
+            {
+                token: "entity.name.function", // String, Array, or Function: the CSS token to apply
+                regex: /\{.*?\}/, // String or RegExp: the regexp to match
+            },
+        ]
+    };
+};
+
+oop.inherits(SynthHighlightRules, TextHighlightRules);
+
+exports.SynthHighlightRules = SynthHighlightRules;
+
+});
+
+ace.define("ace/mode/folding/synth",["require","exports","module","ace/lib/oop","ace/range","ace/mode/folding/fold_mode"], function(require, exports, module) {
+"use strict";
+
+var oop = require("../../lib/oop");
+var Range = require("../../range").Range;
+var BaseFoldMode = require("./fold_mode").FoldMode;
+
+var FoldMode = exports.FoldMode = function() {};
+oop.inherits(FoldMode, BaseFoldMode);
+
+(function() {
+    this.foldingStartMarker = /(\{|\[)[^\}\]]*$|^\s*(\/\*)/;
+    this.foldingStopMarker = /^[^\[\{]*(\}|\])|^[\s\*]*(\*\/)/;
+
+    this.getFoldWidgetRange = function(session, foldStyle, row) {
+        var line = session.getLine(row);
+        var match = line.match(this.foldingStartMarker);
+        if (match) {
+            var i = match.index;
+
+            if (match[1])
+                return this.openingBracketBlock(session, match[1], row, i);
+
+            var range = session.getCommentFoldRange(row, i + match[0].length);
+            range.end.column -= 2;
+            return range;
+        }
+
+    };
+
+}).call(FoldMode.prototype);
+
+});
+
+ace.define("ace/mode/synth",["require","exports","module","ace/lib/oop","ace/mode/text","ace/tokenizer","ace/mode/matching_brace_outdent","ace/mode/synth_highlight_rules","ace/mode/folding/synth"], function(require, exports, module) {
+"use strict";
+
+var oop = require("../lib/oop");
+var TextMode = require("./text").Mode;
+var Tokenizer = require("../tokenizer").Tokenizer;
+var MatchingBraceOutdent = require("./matching_brace_outdent").MatchingBraceOutdent;
+var MyNewHighlightRules = require("./synth_highlight_rules").SynthHighlightRules;
+var MyNewFoldMode = require("./folding/synth").SynthFoldMode;
+
+var Mode = function() {
+    this.HighlightRules = MyNewHighlightRules;
+    this.$outdent = new MatchingBraceOutdent();
+};
+oop.inherits(Mode, TextMode);
+
+(function() {
+    this.lineCommentStart = "//";
+    this.blockComment = {start: "/*", end: "*/"};
+    this.getNextLineIndent = function(state, line, tab) {
+        var indent = this.$getIndent(line);
+        return indent;
+    };
+
+    this.checkOutdent = function(state, line, input) {
+        return this.$outdent.checkOutdent(line, input);
+    };
+
+    this.autoOutdent = function(state, doc, row) {
+        this.$outdent.autoOutdent(doc, row);
+    };
+
+}).call(Mode.prototype);
+
+exports.Mode = Mode;
+});
